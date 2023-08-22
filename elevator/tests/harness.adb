c2843d773bd9fd80161420209ffbfb667d333472
Oliver Lillberg <44681414+xDeZex@users.noreply.github.com>
2023-08-22 14:57:04 +0200

    updated names on tests

--- elevator/tests/harness.adb
+++ elevator/tests/harness.adb
@@ -2,9 +2,9 @@ with AUnit.Reporter.Text;
 with AUnit.Run;
 with Elevator_Suite; use Elevator_Suite;
 
-procedure Test_Building is
+procedure Harness is
    procedure Runner is new AUnit.Run.Test_Runner (Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
 begin
    Runner (Reporter);
-end Test_Building;
+end Harness;
